f7da4a3a29ed0beecedb451f3a7183c515eef01e
Brooklin C <brooklincharlton1@gmail.com>
2025-01-21 19:40:13 +1000

    Commented out Landing Page

--- pingpenguins-frontend/src/main.jsx
+++ pingpenguins-frontend/src/main.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
 import LandingPage from "./pages/LandingPage.jsx";
-
 import NavBar from "./components/NavBar.jsx";
 
 const router = createBrowserRouter([
