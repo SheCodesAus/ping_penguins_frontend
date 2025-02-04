7cde4bedfd5498dc4d30d5eddbd14730d764b5cb
Brooklin C <brooklincharlton1@gmail.com>
2025-02-04 14:48:04 +1000

    Shauna fixes more

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -20,10 +20,21 @@ function NavBar() {
 
     return (
         <div className="app-layout">
-            <nav>
+            <nav className="home-navbar">
+                <div className="nav-container">
+                <div className="nav-logo">  
                 <Link to="/" className="logo">
-                    <img src="/img/logo-crowdbloom.png" alt="StickyBloom Logo" />
+                    <img src="/img/Stickybloomlogo.png" alt="StickyBloom Logo"/>
                 </Link>
+                </div>
+        
+        <div id="rotate-words">
+            <div>Energising Cultures</div>
+            <div>Elevating Happiness</div>
+            <div>Bespoke Culture Strategies</div>
+            <div>Unforgettable Workplace Experiences</div>
+        </div>
+        </div>      
                 <div className="nav-links">
                     {auth.token ? (
                         <Link to="/" onClick={handleLogout}>Log Out</Link>
@@ -33,7 +44,7 @@ function NavBar() {
                                 <Link to="/signup">Sign Up</Link>
                             </>
                     )}
-                </div>
+                </div> 
             </nav>
 
             <main className="main-content">
